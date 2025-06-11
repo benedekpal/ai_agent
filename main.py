523ba1b262e388190c131609fabeaad0e1e1ab79
benedekpal <benedek.pal1024@gmail.com>
2025-06-12 00:56:51 +0200

    revorked argument parsing, added calculator to project

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from google import genai
 from google.genai import types
 import sys
 
-def main(user_prompt):
+def main(user_prompt, verbose):
+    print("verbose", verbose)
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
 
@@ -14,30 +16,42 @@ def main(user_prompt):
 
     client = genai.Client(api_key=api_key)
 
-    try:
-        messages = [
-            types.Content(role="user", parts=[types.Part(text=user_prompt)]),
-        ]
+    user_prompt = " ".join(user_prompt)
 
+    if verbose:
+        print(f"User prompt: {user_prompt}\n")
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    generate_content(client, messages, verbose)
+
+
+def generate_content(client, messages, verbose):
+    try:
         response = client.models.generate_content(
             model='gemini-2.0-flash-001', contents=messages
         )
+        if verbose:
+            print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+            print("Response tokens:", response.usage_metadata.candidates_token_count)
+        print("Response:")
         print(response.text)
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     except Exception as e:
         print(f"Error calling Gemini API: {e}")
         sys.exit(1)
     
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Error: Please provide exactly one prompt argument")
+
+    verbose = "--verbose" in sys.argv
+    args = [arg for arg in sys.argv[1:] if not arg.startswith("--")]
+
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here" [--verbose]')
+        print('Example: python main.py "How do I build a calculator app?"')
         sys.exit(1)
 
-    prompt = sys.argv[1].strip()
-    if not prompt:
-        print("Error: Prompt cannot be empty")
-        sys.exit(1)
-
-    main(prompt)
+    main(args, verbose)
     
