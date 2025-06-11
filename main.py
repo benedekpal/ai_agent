c39b79df9031bda9ec31857e193a79cdaf5d2461
benedekpal <benedek.pal1024@gmail.com>
2025-06-11 21:35:00 +0200

    store prompt in a list so the llm can remember the conversation

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 import sys
 
-def main(prompt):
+def main(user_prompt):
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
 
@@ -14,8 +15,12 @@ def main(prompt):
     client = genai.Client(api_key=api_key)
 
     try:
+        messages = [
+            types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+        ]
+
         response = client.models.generate_content(
-            model='gemini-2.0-flash-001', contents=prompt
+            model='gemini-2.0-flash-001', contents=messages
         )
         print(response.text)
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
